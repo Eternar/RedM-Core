778f8cabfbc77646a8b3a81d88a21cc99c2e43e4
KillStr3aK <statser07@gmail.com>
2021-09-22 00:05:26 +0200

    Add project files.

---
+++ Client/eCombatAbilityLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Eternar.Core.Natives
+{
+    public enum eCombatAbilityLevel : int
+    {
+        CAL_POOR,
+        CAL_AVERAGE,
+        CAL_PROFESSIONAL
+    };
+}
---
+++ Server/Server.cs
@@ -0,0 +1,14 @@
+﻿namespace Eternar.Core.Natives
+{
+    using System.Collections.Generic;
+
+    using CitizenFX.Core.Native;
+
+    public static class Natives
+    {
+        private static readonly Dictionary<string, Hash> NativeHashes = new Dictionary<string, Hash>
+        {
+
+        };
+    }
+}
