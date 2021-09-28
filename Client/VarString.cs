a4f700e691153a11da1e910cc61edf71d754eec1
KillStr3aK <statser07@gmail.com>
2021-09-28 15:37:20 +0200

    Lambda constructor for VarString

--- Client/VarString.cs
+++ Client/VarString.cs
@@ -7,9 +7,7 @@
         public long Text { get; internal set; }
 
         public VarString(long l)
-        {
-            this.Text = l;
-        }
+            => this.Text = l;
 
         public static implicit operator long(VarString vs) => vs.Text;
         public static implicit operator VarString(long l) => new VarString(l);
